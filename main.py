452530aa216ea227c57d6bc2ca99f72d0ea5482e
yajath-p <yajath.parashar@gmail.com>
2026-02-28 21:16:15 -0500

    deletion of function

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
-
-def this_function_will_not_exist():
-    print("delete")
-    
+ 
 def new_function():
     print("new")
